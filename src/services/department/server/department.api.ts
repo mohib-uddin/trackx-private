483d9bc4e506d2ad6812b6e955549011a6e36ec9
mohib-uddin <mohibplayz31@gmail.com>
2024-04-25 06:14:57 -0700

    chore:base-uri

--- src/services/department/server/department.api.ts
+++ src/services/department/server/department.api.ts
@@ -1,4 +1,6 @@
 import serverFetch from "@/_utils/config/server-fetch";
+// import { fetchEmployeeByDepartment } from "@/_utils/types/department";
+// @ts-ignore
 import { fetchEmployeeByDepartment } from "@/_utils/types/department";
 
 export function fetchEmployeeByDepartment(
